06efc4ddd1ecfbbd28d54a9e86f493bffdc9dde6
codedholes <codedholes@gmail.com>
2017-02-18 21:49:38 +0100

    001A0

---
+++ Grapher/BMAP.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Grapher
+{
+    public class BMAP
+    {
+        public static List<int> Ids = new List<int>();
+
+        public static int width = 512, height = width;
+
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public BMAP()
+        { }
+
+        public BMAP(int id, string value)
+        { Id = id; Value = value; }
+
+        public static Bitmap GetMap(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    return PrimeDistribution();
+            }
+            return null;
+        }
+
+        public static BitmapImage GetMapImage(int id)
+        { return BitmapImageFromBitmap(GetMap(id)); }
+
+        #region conversions
+        public static Bitmap BitmapFromWriteableBitmap(WriteableBitmap writewbmp)
+        {
+            Bitmap wbmp;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(writewbmp));
+                enc.Save(outStream);
+                wbmp = new Bitmap(outStream);
+            }
+            return wbmp;
+        }
+
+        public static BitmapImage BitmapImageFromBitmap(Bitmap iwbmp)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                Bitmap wbmp = new Bitmap(iwbmp);
+                wbmp.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            return bitmapImage;
+        }
+        #endregion
+
+        //ID = 0
+        public static Bitmap PrimeDistribution()
+        {
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format16bppRgb555);
+            Random rand = new Random();
+            Color b = Color.White;
+            for (int by = 0; by < height; by++)
+                for (int bx = 0; bx < width; bx++)
+                    bmp.SetPixel(bx, by, b);
+            Color c = Color.Black;
+            int ll = 1;
+
+            int x = width / 2;
+            int y = height / 2;
+
+            int i;
+            int j = 1;
+
+            for (int a = 0; a < width / 2 - 1 && a < height / 2 - 1; a++)
+            {
+//                Console.WriteLine(j + "/" + (width < height ? width * width : height * height));
+                for (i = 0; i < ll; i++)
+                {
+                    if (isPrime(j))
+                    { bmp.SetPixel(x + i, y, c); }
+                    j++;
+                }
+                x += i;
+                for (i = 0; i < ll; i++)
+                {
+                    if (isPrime(j))
+                    { bmp.SetPixel(x, y - i, c); }
+                    j++;
+                }
+                y -= i;
+                ll++;
+                for (i = 0; i < ll; i++)
+                {
+                    if (isPrime(j))
+                    { bmp.SetPixel(x - i, y, c); }
+                    j++;
+                }
+                x -= i;
+                for (i = 0; i < ll; i++)
+                {
+                    if (isPrime(j))
+                    { bmp.SetPixel(x, y + i, c); }
+                    j++;
+                }
+                y += i;
+                ll++;
+            }
+            bmp.SetPixel(width / 2, height / 2, Color.Red);
+
+            Console.WriteLine(bmp.Width + ":" + bmp.Height);
+            return bmp;
+        }
+
+        public static bool isPrime(BigInteger c)
+        {
+            if ((c & 1) == 0)
+            {
+                if (c == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            for (BigInteger i = 3; (i * i) <= c; i += 2)
+            {
+                if ((c % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return c != 1;
+        }
+    }
+}
---
+++ Grapher/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Grapher
+{
+    public partial class MainWindow : Window
+    {
+        public static List<BMAP> type = new List<BMAP>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Plottable.MinWidth = BMAP.width;
+            //Plottable.MinHeight = BMAP.height;
+
+            BMAP.Ids.Add(0);
+            BMAP.Ids.Add(1);
+
+            type.Add(new BMAP(0, "Prime distribution"));
+            type.Add(new BMAP(1, "Pi digit distribution"));
+
+            PlotSourceBox.DisplayMemberPath = "Value";
+            PlotSourceBox.SelectedValuePath = "Id";
+            PlotSourceBox.ItemsSource = type;
+        }
+
+        static int i;
+        private void PlotButton_Click(object sender, RoutedEventArgs e)
+        {
+            BMAP.width = BMAP.height = Int32.Parse(PlotIterationBox.Text);
+            PlotStatLabel.Content = BMAP.width * BMAP.height + " iterations.";
+            i = (int)PlotSourceBox.SelectedValue;
+            ImageSource imageSource = BMAP.GetMapImage((int)PlotSourceBox.SelectedValue);
+            Plottable.Source = imageSource;
+        }
+
+        private void PlotSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            BMAP.width = BMAP.height = Int32.Parse(PlotIterationBox.Text);
+            PlotStatLabel.Content = BMAP.width * BMAP.height + " iterations.";
+            Bitmap bmp = BMAP.GetMap(i);
+            ImageSource imageSource = BMAP.BitmapImageFromBitmap(bmp);
+            Plottable.Source = imageSource;
+            bmp.Save(PlotSourceBox.SelectedItem + ".png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void PlotIterationBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private static bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            return !regex.IsMatch(text);
+        }
+
+        private void PlotIterationBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+    }
+}
